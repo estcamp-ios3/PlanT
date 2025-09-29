a6449511c62bc5887084cd96e3b5f4fc76be0a47
akarisoho <akarisoho1@gmail.com>
2025-09-29 22:46:35 +0900

    feat: 새로 쓰기 버튼 스타일 추가, DesignTokens-스타일 기본값-추가

--- PlanT/PlanT/Common/CommonStyleDemo.swift
+++ PlanT/PlanT/Common/CommonStyleDemo.swift
@@ -9,68 +9,91 @@ import SwiftUI
 
 struct CommonStyleDemo: View {
     var body: some View {
-        VStack {
-            HStack(spacing: 16) {
-                Button {
-                    // action
+        ScrollView {
+            VStack(spacing: vertical5) {
+                // MARK: - 주기능 버튼
+                HStack(spacing: vertical3) {
+                    Button { /* action */
+                    } label: {
+                        Text("Primary")
+                    }
+                    .plantPrimaryButton()
+                    
+                    Button { /* action */
+                    } label: {
+                        Text("Disabled")
+                    }
+                    .plantPrimaryButton()
+                    .disabled(true)
                 }
-                label:{
-                    Text("Primary")
-                }
-                .plantPrimaryButton()
                 
-                Button { }
-                label:{
-                    Text("Disabled")
+                // MARK: - 부기능 버튼
+                HStack(spacing: vertical3) {
+                    Button { /* action */
+                    } label: {
+                        Text("Secondary")
+                    }
+                    .plantSecondaryButton()
+                    
+                    Button { /* action */
+                    } label: {
+                        Text("Disabled")
+                    }
+                    .plantSecondaryButton()
+                    .disabled(true)
                 }
-                .plantPrimaryButton()
-                .disabled(true)
-            }
-            
-            HStack(spacing: 16) {
-                Button { }
-                label:{
-                    Text("Secondary")
-                }
-                .plantSecondaryButton()
                 
-                Button { }
-                label:{
-                    Text("Disabled")
+                // MARK: - 이미지는 비율에 맞춰 frame 사이즈 안에 잘리지 않도록 넣기
+                HStack(alignment: .top) {
+                    Image("sun_shine")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    
+                    Image("seed_Sunflower05")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250, height: 400)
                 }
-                .plantSecondaryButton()
-                .disabled(true)
-            }
-            
-            Spacer()
-            
-            VStack{
-                Button { }
-                label:{
+                
+                // MARK: - 로그인 예시
+                Button { /* action */
+                } label: {
                     Text("로그인")
                 }
                 .plantPrimaryButton()
-//                .disabled(userName.isEmpty || password.isEmpty))
-            }
-            
-            Spacer()
-            
-            HStack(spacing: 16) {
-                Button { }
-                label:{
-                    Text("취소")
-                }
-                .plantSecondaryButton()
+                // .disabled(userName.isEmpty || password.isEmpty))
                 
-                Button { }
-                label:{
-                    Text("저장")
+                Spacer()
+                
+                // MARK: - 확인/취소 예시
+                HStack(spacing: vertical3) {
+                    Button { /* action */
+                    } label: {
+                        Text("취소")
+                    }
+                    .plantSecondaryButton()
+                    
+                    Button { /* action */
+                    } label: {
+                        Text("저장")
+                    }
+                    .plantPrimaryButton()
                 }
-                .plantPrimaryButton()
             }
         }
         .padding()
         .background(Color(.systemBackground).ignoresSafeArea())
+        // MARK: - 새로 만들기 버튼
+        .overlay(alignment: .bottomTrailing) {
+            Button { /* action */
+            } label: {
+                Image(systemName: "plus")
+            }
+            .plantFABStyle(diameter: 56, iconSize: 30, useAccent: true)
+            .padding(20)
+            .padding(.bottom, 68)
+        }
     }
 }
 #Preview("Light") {
--- PlanT/PlanT/Views/RoutineTodoListView/RoutineListView.swift
+++ PlanT/PlanT/Views/RoutineTodoListView/RoutineListView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct RoutineListView: View {
     var body: some View {
         ScrollView {
-            Text("여기에 루틴이 추가됩니다.")
+            VStack(spacing: vertical5) {
+                Text("Routine List")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("여기에 루틴이 추가됩니다.")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+
         }
-        
+        .padding(20)
+        .overlay(alignment: .bottomTrailing) {
+            Button { /* action */ } label: { Image(systemName: "plus") }
+                .plantFABStyle(diameter: 56, iconSize: 30, useAccent: true)
+                .padding(20)
+        }
     }
 }
 
