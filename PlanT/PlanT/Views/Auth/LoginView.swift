26ef1819c3e2e39961ae706ee5b40296287d1e05
TinyJiHun <tinyjihun@gmail.com>
2025-09-29 15:05:01 +0900

    작업중

--- PlanT/PlanT/Views/Auth/LoginView.swift
+++ PlanT/PlanT/Views/Auth/LoginView.swift
@@ -4,4 +4,14 @@
 //
 //  Created by catharina J on 9/29/25.
 //
+import SwiftUI
 
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview() {
+    LoginView()
+}
--- PlanT/PlanT/Views/Auth/SignUpView.swift
+++ PlanT/PlanT/Views/Auth/SignUpView.swift
@@ -5,3 +5,14 @@
 //  Created by catharina J on 9/29/25.
 //
 
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview() {
+    SignUpView()
+}
