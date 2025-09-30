30510dd3017bed649a998eff7157c24c9b48b7e2
gwanoove <bestmd0320@gmail.com>
2025-09-30 17:40:27 +0900

    refactor:  navigationStack
    modal

--- PlanT/PlanT/Views/SeedRouter/SeedStatusView.swift
+++ PlanT/PlanT/Views/SeedRouter/SeedStatusView.swift
@@ -10,64 +10,86 @@ import SwiftUI
 struct SeedStatusView: View {
     let state: SeedStatus
     @State private var showSeedSelection = false
+    @State private var selectedSeed: Seed? = nil
+    @State private var goToRoutineList = false
     
     var body: some View {
         VStack(spacing: 24) {
             // 상태에 따라 텍스트 변경
             Spacer(minLength: 100) // 위쪽 빈칸
             
-            Text(statusMessage)
-                .font(.title)
-                .lineLimit(nil)
+            if let seed = selectedSeed {
+                (
+                    Text("\(seed.name)")
+                        .foregroundColor(.orange)
+                        .font(.title)
+                    + Text(" 씨앗을 선택하셨습니다.\n루틴을 등록할까요?")
+                        .foregroundColor(.black)
+                        .font(.title)
+                )
                 .multilineTextAlignment(.center)
-                .foregroundColor(.black)
                 .padding(.horizontal, 16)
-            Spacer()
-            // 가운데 흙/씨앗 이미지
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 300) // 원하는 크기로 조정
-                .padding(.bottom, 12)
-            
-            // 아래 버튼
-            Button {
-                showSeedSelection = true
-            } label: {
-                Text("씨앗선택하기")
+                .lineLimit(nil)
+                .fixedSize(horizontal: false, vertical: true)
+                
+                Spacer()
+                
+                Image(seed.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300) // 원하는 크기로 조정
+                    .padding(.bottom, 12)
+                
+                Spacer()
+                
+                Button("씨앗 다시 선택하기") {
+                    showSeedSelection = true
+                }
+                .plantSecondaryButton()
+                .padding(.horizontal, 20)
+                
+                Button("등록하기") {
+                    goToRoutineList = true
+                }
+                .plantPrimaryButton()
+                .padding(.horizontal, 20)
+                
+                Spacer(minLength: 40)   // 버튼 그룹 밑에 빈칸
+                
+            } else {
+                Text("아직 심겨진 씨앗이 없어요!\n새로운 씨앗을 심어볼까요?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+                Spacer()
+                
+                Image("Soil")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:300, height: 300)
+                    .padding(.bottom,12)
+                Spacer()
+                
+                Button("씨앗 선택하기") {
+                    showSeedSelection = true
+                }
+                .plantPrimaryButton()
+                .padding(.horizontal, 20)
             }
-            .plantPrimaryButton()
-            .padding(.horizontal, 20)
-            Spacer(minLength: 50) // 아래쪽 빈칸
-            
+            Spacer(minLength: 50)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
-        .sheet(isPresented: $showSeedSelection) {
-            SeedSelectionView()
+        .navigationDestination(isPresented: $goToRoutineList) {
+            RoutineListView()
         }
-        
-    }
-    
-    private var statusMessage: String {
-        switch state {
-        case .planted(_):
-            return "현재 (닉네임)님이 키우고 있는\n작물은 (몇) 개 입니다"
-        case .notPlanted:
-            return "아직 심겨진 씨앗이 없어요!\n새로운 씨앗을 심어볼까요?"
-        }
-    }
-    
-    private var imageName: String {
-        switch state {
-        case .planted:
-            return "SoilSprout" // ex: 심긴 이미지 이름
-        case .notPlanted:
-            return "Soil" // ex: 안 심긴 상태 이미지
+        .sheet(isPresented: $showSeedSelection, onDismiss: {
+        }) {
+            SeedSelectionView(selectedSeed: $selectedSeed)
         }
     }
 }
-
 
 #Preview("미심기 상태") {
     SeedStatusView(state: .notPlanted)
