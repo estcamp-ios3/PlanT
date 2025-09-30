c121fb84866ca956446fb4593f4b4e0ee0a1ea7a
TinyJiHun <tinyjihun@gmail.com>
2025-09-30 11:16:35 +0900

    모디파이어 시스템에 맞게 수정

--- PlanT/PlanT/Models/Auth/UserAuthModel.swift
+++ PlanT/PlanT/Models/Auth/UserAuthModel.swift
@@ -16,20 +16,40 @@ final class UserAuthModel: ObservableObject {
     @Published var password: String = ""
 }
 
+
 // 텍스트 필드 모디파이어 임시
 struct AuthTextFieldModifier: ViewModifier {
+    enum Style {
+        case signIn // 로그인 텍스트 필드
+        case signUp // 회원가입 텍스트 필드
+    }
+    
+    var style: Style
+    
     func body(content: Content) -> some View {
-        content
-            .padding()
-            .background(Color(.systemGray6))
-            .cornerRadius(24)
-            .textInputAutocapitalization(.never)
-            .autocorrectionDisabled(true)
+        switch style {
+        case .signIn:
+            content
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 24)
+                        .stroke(Color.gray400, lineWidth: 2)
+                )
+                .font(.system(size: 16, weight: .bold))
+        case .signUp:
+            content
+                .padding(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 24)
+                        .stroke(Color.gray400, lineWidth: 2)
+                )
+                .font(.system(size: 16, weight: .bold))
+        }
     }
 }
 
 extension View {
-    func authTextFieldStyle() -> some View {
-        self.modifier(AuthTextFieldModifier())
+    func authTextFieldStyle(_ style: AuthTextFieldModifier.Style = .signIn) -> some View {
+        self.modifier(AuthTextFieldModifier(style: style))
     }
 }
