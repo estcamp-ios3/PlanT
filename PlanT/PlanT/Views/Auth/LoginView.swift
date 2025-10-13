9393f1e7a1b46085681732f907897ba4c299d9d3
TinyJiHun <tinyjihun@gmail.com>
2025-10-13 17:48:30 +0900

    텍스트 필드 밑 버튼 간격 조정

--- PlanT/PlanT/Views/Auth/LoginView.swift
+++ PlanT/PlanT/Views/Auth/LoginView.swift
@@ -44,6 +44,7 @@ struct LoginView: View {
                 SecureField("Password", text: $viewModel.password)
                     .authTextFieldStyle(.signIn)
                     .focusRoute($focus, equals: .pw, submit: .go, next: nil)
+                    .padding(.bottom, 8)
 
                 Button {
                     Task { await viewModel.signIn() }
@@ -56,6 +57,7 @@ struct LoginView: View {
                 }
                 .plantPrimaryButton()
                 .disabled(viewModel.isLoading)
+                .padding(.bottom, 16)
 
                 Button("Sign Up") { isPresentingSignUp = true }
                     .foregroundColor(.black)
