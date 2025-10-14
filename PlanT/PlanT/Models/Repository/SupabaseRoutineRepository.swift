f52632f4c2b1f691242c98927239954b04208a46
gwanoove <bestmd0320@gmail.com>
2025-10-14 16:06:17 +0900

    refactor: customToggleStyle, routine Supabass
    data save

--- PlanT/PlanT/Models/Repository/SupabaseRoutineRepository.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SupabaseRoutineRepository.swift
-//  PlanT
-//
-//  Created by 박성관 on 10/7/25.
-//
-
--- PlanT/PlanT/Models/RoutineTemplate/Routine.swift
+++ PlanT/PlanT/Models/RoutineTemplate/Routine.swift
@@ -11,7 +11,7 @@ import SwiftData
 // MARK: - 루틴 모델
 // 개별 루틴 하나를 표현하는 데이터
 @Model
-final class Routine {
+final class Routine: Identifiable {
     var id = UUID()
     var title: String
     var detail: RoutineDetail
@@ -21,8 +21,7 @@ final class Routine {
     var isCompleted: Bool = false
     var createdAt: Date
     var modifiedAt: Date
-    
-    
+
     init(
         id: UUID = UUID(),
         title: String,
@@ -44,4 +43,51 @@ final class Routine {
         self.createdAt = createdAt
         self.modifiedAt = modifiedAt
     }
+}
+
+struct RoutineDTO: Codable {
+    var id: UUID
+    var title: String
+    var categoryId: String
+    var seedName: String?
+    var note: String?
+    var isCompleted: Bool
+    var duration: String
+    var goal: String
+    var alarm: String
+    var createdAt: Date
+    var modifiedAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+            case id
+            case title
+            case categoryId = "category_id"
+            case seedName = "seed_name"
+            case note
+            case isCompleted = "is_completed"
+            case duration
+            case goal
+            case alarm
+            case createdAt = "created_at"
+            case modifiedAt = "modified_at"
+        }
+}
+
+extension Routine {
+    var dto: RoutineDTO {
+        RoutineDTO(
+            id: id,
+            title: title,
+            categoryId: categoryId,
+            seedName: seedName,
+            note: note,
+            isCompleted: isCompleted,
+            duration: detail.duration,
+            goal: detail.goal,
+            alarm: detail.alarm.rawValue,
+            createdAt: createdAt,
+            modifiedAt: modifiedAt
+            )
+    }
+
 }
