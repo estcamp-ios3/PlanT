985bf190e1dd482b1f33a6237a6a0437987ba18a
TinyJiHun <tinyjihun@gmail.com>
2025-10-16 13:04:51 +0900

    사용자설정 화면 준비중

--- PlanT/PlanT/Views/Mypage/SubView/SettingView.swift
+++ PlanT/PlanT/Views/Mypage/SubView/SettingView.swift
@@ -5,3 +5,16 @@
 //  Created by 이지훈 on 10/16/25.
 //
 
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        VStack {
+            Text("SettingView")
+        }
+    }
+}
+
+#Preview {
+    SettingView()
+}
