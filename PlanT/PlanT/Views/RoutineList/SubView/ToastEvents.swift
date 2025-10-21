637cb7ee9b94ba5e238a3d8de29a4066733aa0ad
TinyJiHun <tinyjihun@gmail.com>
2025-10-21 13:07:58 +0900

    토스트메시지 이벤트 호출

--- PlanT/PlanT/Views/RoutineList/SubView/ToastEvents.swift
+++ PlanT/PlanT/Views/RoutineList/SubView/ToastEvents.swift
@@ -8,11 +8,14 @@
 import Foundation
 
 extension Notification.Name {
+    /// Mate 토스트를 특정 메시지로 보여달라는 이벤트
+    /// userInfo: ["message": String]
     static let showMateToast = Notification.Name("showMateToast")
 }
 
 /// 간편 호출용 헬퍼
 enum MateToastCenter {
+    /// Mate 토스트 메시지를 전역으로 뿌림
     static func show(_ message: String) {
         NotificationCenter.default.post(
             name: .showMateToast,
