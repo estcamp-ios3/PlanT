293d7f75a0c770e41caea0f381396ea70c5be2ef
TinyJiHun <tinyjihun@gmail.com>
2025-10-21 11:09:27 +0900

    미사용으로 인해 전체 주석 처리(삭제예정)

--- PlanT/PlanT/Models/Alan/AlanAIExample.swift
+++ PlanT/PlanT/Models/Alan/AlanAIExample.swift
@@ -1,69 +1,69 @@
+////
+////  AlanAIExample.swift
+////  PlanT
+////
+////  Created by 이지훈 on 10/17/25.
+////
 //
-//  AlanAIExample.swift
-//  PlanT
+//import SwiftUI
+//import AlanAI
 //
-//  Created by 이지훈 on 10/17/25.
+//struct AlanAIExample: View {
+//    @State private var contextList: [AttributedString] = []
+//    @State private var prompt: String = "오늘 서울 날씨 어때?"
+//    
+//    var body: some View {
+//        VStack {
+//            List(contextList, id: \.self) { text in
+//                Text(text)
+//            }
+//            .listStyle(.plain)
+//            
+//            Divider()
+//            
+//            HStack {
+//                TextField("Prompt", text: $prompt)
+//                    .lineLimit(5)
+//                Button("Send") {
+//                    Task {
+//                        await sendPrompt()
+//                    }
+//                }
+//            }
+//            .padding()
+//        }
+//    }
+//    
+//    private func sendPrompt() async {
+//        
+//        contextList.append(attributedString("**\(prompt)**"))
+//        contextList.append(attributedString("(응답 생성중...)"))
+//        
+//        let clientId = "89a3432c-e591-4751-9b97-2d4fc2d9fa6a" // 지훈 키값
+//        let alanAI = AlanAI(clientID: clientId)
+//        
+//        do {
+//            let response: AlanResponse? = try await alanAI.question(query: prompt)
+//            
+//            if let response {
+//                contextList.removeLast(1)
+//                contextList.append(attributedString(response.content ?? "(none)"))
+//            }
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+//    }
+//    
+//    private func attributedString(_ text: String) -> AttributedString {
+//        let attributed = try! AttributedString(
+//            markdown: text,
+//            options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)
+//        )
+//        
+//        return attributed
+//    }
+//}
 //
-
-import SwiftUI
-import AlanAI
-
-struct AlanAIExample: View {
-    @State private var contextList: [AttributedString] = []
-    @State private var prompt: String = "오늘 서울 날씨 어때?"
-    
-    var body: some View {
-        VStack {
-            List(contextList, id: \.self) { text in
-                Text(text)
-            }
-            .listStyle(.plain)
-            
-            Divider()
-            
-            HStack {
-                TextField("Prompt", text: $prompt)
-                    .lineLimit(5)
-                Button("Send") {
-                    Task {
-                        await sendPrompt()
-                    }
-                }
-            }
-            .padding()
-        }
-    }
-    
-    private func sendPrompt() async {
-        
-        contextList.append(attributedString("**\(prompt)**"))
-        contextList.append(attributedString("(응답 생성중...)"))
-        
-        let clientId = "89a3432c-e591-4751-9b97-2d4fc2d9fa6a" // 지훈 키값
-        let alanAI = AlanAI(clientID: clientId)
-        
-        do {
-            let response: AlanResponse? = try await alanAI.question(query: prompt)
-            
-            if let response {
-                contextList.removeLast(1)
-                contextList.append(attributedString(response.content ?? "(none)"))
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-    
-    private func attributedString(_ text: String) -> AttributedString {
-        let attributed = try! AttributedString(
-            markdown: text,
-            options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)
-        )
-        
-        return attributed
-    }
-}
-
-#Preview {
-    AlanAIExample()
-}
+//#Preview {
+//    AlanAIExample()
+//}
