c310d075745aabce600b6b47430e3e26c0a99d68
TinyJiHun <tinyjihun@gmail.com>
2025-10-23 15:16:34 +0900

    유저카드 고정, 밑에 스크롤뷰로 추가 설정 불러오게 레이아웃 설정

--- PlanT/PlanT/Views/Mypage/SubView/SettingView.swift
+++ PlanT/PlanT/Views/Mypage/SubView/SettingView.swift
@@ -12,21 +12,21 @@ struct SettingView: View {
     @State private var showSignOutAlert = false
 
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack {
-                    SettingUserCardView(authStore: authStore)
-                }
+        VStack {
+            HStack {
+                SettingUserCardView(authStore: authStore)
             }
-            .padding(.horizontal, vertical4)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        showSignOutAlert = true
-                    } label: {
-                        Image(systemName: "door.left.hand.open")
-                            .foregroundColor(.red)
-                    }
+            ScrollView {
+            }
+        }
+        .padding(.horizontal, vertical4)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    showSignOutAlert = true
+                } label: {
+                    Image(systemName: "door.left.hand.open")
+                        .foregroundColor(.red)
                 }
             }
         }
